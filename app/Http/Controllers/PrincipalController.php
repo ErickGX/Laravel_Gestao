fc242adf31f7a65f0cf802cad8910faaacc7042e
ErickGX <ericksaopaulino1@hotmail.com>
2023-06-20 21:54:11 -0300

    Adicionando rotas e criando controllers no Laravel

---
+++ app/Http/Controllers/PrincipalController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PrincipalController extends Controller
+{
+    public function principal() {
+        echo 'Pagina inicial Seja Bem vindo ao Super gestão';
+    }
+
+
+    
+}
